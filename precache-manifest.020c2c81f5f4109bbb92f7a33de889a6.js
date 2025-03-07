07050b37d5ada73fd138e74366a0d80e3846b39e
Badih Barakat <88603442+badih76@users.noreply.github.com>
2025-03-07 17:08:32 +1100

    Add files via upload
    First upload of ESM website

---
+++ precache-manifest.020c2c81f5f4109bbb92f7a33de889a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9b55c476a90eed97ea020f52b6a63d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d3687511479f88ddffe",
+    "url": "/static/css/main.9f9dee0c.chunk.css"
+  },
+  {
+    "revision": "ba429e771fb6eb9e3a3d",
+    "url": "/static/js/2.8e80d237.chunk.js"
+  },
+  {
+    "revision": "5d3687511479f88ddffe",
+    "url": "/static/js/main.d9aafe6f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "dbe67f1d48c7b790783e84330c0aee7e",
+    "url": "/static/media/ems-approach.dbe67f1d.png"
+  },
+  {
+    "revision": "bfba2972ea3ccad09deac7b6088eac07",
+    "url": "/static/media/ems-standards.bfba2972.png"
+  },
+  {
+    "revision": "942fceae9a5174e9b0909b0d9bbf6f2e",
+    "url": "/static/media/facility-1.942fceae.jpg"
+  },
+  {
+    "revision": "40553e5bec1de0ea577ed7bb7aff446e",
+    "url": "/static/media/facility-10.40553e5b.png"
+  },
+  {
+    "revision": "d3c4ecbf555a179a8b4991e05fa18dd0",
+    "url": "/static/media/facility-11.d3c4ecbf.jpg"
+  },
+  {
+    "revision": "e23a7c6bde6d2a150b0ecaf33267044b",
+    "url": "/static/media/facility-8.e23a7c6b.jpeg"
+  },
+  {
+    "revision": "cf4f6af174406c89eaa7bb7c544daf8a",
+    "url": "/static/media/fmbanner.cf4f6af1.jpg"
+  }
+]);
